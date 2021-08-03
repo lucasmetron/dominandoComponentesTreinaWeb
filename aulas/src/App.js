f3438145fcde0cec22c6e515bd603b338a9e751a
lucasmetron <lucasmetron@gmail.com>
2021-08-03 15:16:20 -0300

    ori

--- aulas/src/App.js
+++ aulas/src/App.js
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
 
     const nome = 'TreinaWeb';
-    const nome2 = 'Hello'
     const MeuComponenteEscolhido = MeusComponentes[nome]; {/*Forma de usar montando um componente com uma variável*/ }
-    const HelloComponent = MeusComponentes[nome2];
 
     return (
 
@@ -16,9 +14,7 @@ class App extends Component {
 
         <MeuComponentente />
         <MeusComponentes.TreinaWeb /> {/*Forma de usar montando um componente com um objeto*/}
-        <MeusComponentes.Hello />
         <MeuComponenteEscolhido /> {/*Forma de usar montando um componente com uma variável*/}
-        <HelloComponent />
 
       </div>
 
