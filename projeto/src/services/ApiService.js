8953a17885cb31a4bd60817339622cfac1792a32
lucasmetron <lucasmetron@gmail.com>
2021-08-04 16:58:04 -0300

    inserido form

--- projeto/src/services/ApiService.js
+++ projeto/src/services/ApiService.js
@@ -3,15 +3,21 @@ const api = "http://localhost:3002/api/react-videos/";
 
 export const ApiService = {
     get(endpoint) {
-        return fetch(`${api}${endpoint}`)
+        return fetch(`${api}/${endpoint}`)
             .then(res => res.json());
     },
 
-    post(endpoint, nome, img, video) {
-        return fetch(`${api}${endpoint}`, {
-            method: 'PUT',
+    post(endpoint, data) {
+
+        return fetch(`${api}/${endpoint}`, {
+            method: 'POST',
+            body: JSON.stringify(data)
 
         })
-            .then(res => res.json());
+            .then(res => {
+                console.log(res)
+                return res.json()
+            })
+
     }
 }
