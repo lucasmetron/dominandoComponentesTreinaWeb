ef098794de1f69a147b68e4fa6eff0757736f9cf
lucasmetron <lucasmetron@gmail.com>
2021-08-04 15:06:13 -0300

    escopo e bind

--- aulas/src/components/MyButton.js
+++ aulas/src/components/MyButton.js
@@ -4,21 +4,26 @@ export default class MyButton extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            teste: 5,
+            isOn: true,
         }
 
-        this.sayHello = this.sayHello.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     }
 
 
-    sayHello(event) {
-        event.preventDefault();
-        alert('clicado')
+    handleClick() {
+        this.setState((state, props) => {
+            console.log(state)
+            return { isOn: !state.isOn }
+        })
+        console.log(this.state.isOn)
     }
 
     render() {
+        const { state } = this;
         return (
-            <button onClick={this.sayHello} > teste</button>
+
+            <button onClick={this.handleClick} >{state.isOn === true ? "Ligado" : "Desligado"}</button>
         );
     }
 }
