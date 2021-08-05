ed050d0abd879703f557238c4b331bf18e2d2fce
lucasmetron <lucasmetron@gmail.com>
2021-08-05 15:10:56 -0300

    parametros de funções

---
+++ aulas/src/components/PeopleList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+const myList = [
+    { nome: "João", idade: 23 },
+    { nome: "Maria", idade: 25 },
+    { nome: "Paulo", idade: 32 },
+    { nome: "Bruna", idade: 20 },
+];
+
+class PeopleList extends Component {
+
+    sayMyName(person) {
+        alert(person.nome)
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {myList.map((item, i) => <li onClick={this.sayMyName.bind(this, item)} key={i}>{item.nome} - {item.idade} anos</li>)}
+                </ul>
+            </div>
+
+
+        );
+    }
+}
+
+export default PeopleList
