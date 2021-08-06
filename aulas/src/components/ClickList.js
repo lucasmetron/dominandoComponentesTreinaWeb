5a114a38a5d104900a5ac154fdf434ef982de5e4
lucasmetron <lucasmetron@gmail.com>
2021-08-06 15:54:34 -0300

    props

--- aulas/src/components/ClickList.js
+++ aulas/src/components/ClickList.js
@@ -9,7 +9,15 @@ class ClickList extends Component {
             <ul>
                 <div>abc</div>
                 {this.props.children.map(item => {
-                    return <li>{item}</li>
+                    return (
+                        <li>
+                            <item.type prop2={this.props.number} > {/*o type pega o tipo do item e cria um novo componente */}
+                                {item.props.children}
+                                {this.props.number}
+                            </item.type>
+                        </li>
+
+                    );
                 })}
             </ul>
 
